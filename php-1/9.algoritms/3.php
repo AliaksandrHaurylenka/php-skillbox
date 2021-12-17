97284d8ac98bc51655b6065c0ea71f4da9345b1c
Aliaksandr <goric0312@mail.ru>
2021-12-17 12:44:23 +0300

    php-1-9

--- php-1/9.algoritms/3.php
+++ php-1/9.algoritms/3.php
@@ -10,10 +10,10 @@ $sum = 0;//заводим переменную, в которую кладем 0
 
 //циклом проходим по ключам и если он не четный, то сумму увеличиваем
 foreach ($numbers as $key => $value) {
-    if($key % 2 != 0) $sum += $value;
+    if($key % 2 != 0) {
+        $sum += $value;
+    }
 }
   
-
-
 var_dump($numbers);
 var_dump($sum);
--- php-1/9.algoritms/5.php
+++ php-1/9.algoritms/5.php
@@ -5,6 +5,8 @@ if ($studentsCount % 10 == 1) {
     echo "На учебе " . $studentsCount . " студент";
 } elseif ($studentsCount % 10 == 2 || $studentsCount % 10 == 3 || $studentsCount % 10 == 4) {
     echo "На учебе " . $studentsCount . " студента";
+}  elseif ($studentsCount % 100 >= 14 || $studentsCount % 100 <= 19) {
+    echo "На учебе " . $studentsCount . " студентов";
 } else {
     echo "На учебе " . $studentsCount . " студентов";
 }

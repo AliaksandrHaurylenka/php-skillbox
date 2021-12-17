763a17b8d60461728493aa257564234e3cd7b96b
Aliaksandr <goric0312@mail.ru>
2021-12-17 14:01:09 +0300

    php-1-9

--- php-1/9.algoritms/5.php
+++ php-1/9.algoritms/5.php
@@ -1,12 +1,12 @@
 <?php
-$studentsCount = rand(1, 100000);
+$studentsCount = rand(1, 1000000);
 
-if ($studentsCount % 10 == 1) {
-    echo "На учебе " . $studentsCount . " студент";
+if ($studentsCount % 100 >= 11 && $studentsCount % 100 <= 19) {
+    echo "На учебе " . $studentsCount . " студентов";
 } elseif ($studentsCount % 10 == 2 || $studentsCount % 10 == 3 || $studentsCount % 10 == 4) {
     echo "На учебе " . $studentsCount . " студента";
-}  elseif ($studentsCount % 100 >= 14 || $studentsCount % 100 <= 19) {
-    echo "На учебе " . $studentsCount . " студентов";
+} elseif ($studentsCount % 10 == 1) {
+    echo "На учебе " . $studentsCount . " студент";
 } else {
     echo "На учебе " . $studentsCount . " студентов";
 }
